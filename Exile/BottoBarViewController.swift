cf1f708f9883b54210f9facd89f733165121b276
Yamid Pico Leal <ypicoleal@gmail.com>
2017-06-02 19:06:52 -0500

    agregando cargando a vista de login

---
+++ Exile/BottoBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BottoBarViewController.swift
+//  Exile
+//
+//  Created by Exile Soluciones Tecnológicas on 2/06/17.
+//  Copyright © 2017 Exile Soluciones. All rights reserved.
+//
+import UIKit
+
+class BottomBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- Exile/LoginViewController.swift
+++ Exile/LoginViewController.swift
@@ -12,6 +12,13 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var loginField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    @IBOutlet weak var loginContainer: UIView!
+    @IBOutlet weak var passwordContainer: UIView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,11 +39,19 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         }else {
             self.view.endEditing(true)
         }
+        errorLabel.isHidden = true
         
         return false
     }
     
     @IBAction func onLongin(_ sender: UIButton) {
+        loginContainer.isHidden = true
+        passwordContainer.isHidden = true
+        usernameLabel.isHidden = true
+        passwordLabel.isHidden = true
+        loginButton.isHidden = true
+        errorLabel.isHidden = true
+        indicator.startAnimating()
         do{
             let json = ["username": loginField.text, "password": passwordField.text]
             let data = try JSONSerialization.data(withJSONObject: json, options: JSONSerialization.WritingOptions.prettyPrinted)
@@ -52,17 +67,26 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             let task =  URLSession.shared.dataTask(with: request as URLRequest, completionHandler: {
                 (data, response, error) -> Void in
                 if let data = data {
-                                                    
-                    // You can print out response object
-                    print("******* response = \(String(describing: response))")
-                                                    
                     print(data.count)
                     let responseString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
                     print("****** response data = \(responseString!)")
-                    
-                    let mainView = self.storyboard?.instantiateViewController(withIdentifier: "MainView")
-                    self.presentedViewController(mainView, animated: true)
-                    
+                    DispatchQueue.main.async(execute: {
+                        self.indicator.stopAnimating()
+                        if let httpResponse = response as? HTTPURLResponse {
+                            if(httpResponse.statusCode == 200){
+                                let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+                                let resultViewController = storyBoard.instantiateViewController(withIdentifier: "MainView") as! BottomBarViewController
+                                self.present(resultViewController, animated:true, completion:nil)
+                            }else{
+                                self.loginContainer.isHidden = false
+                                self.passwordContainer.isHidden = false
+                                self.usernameLabel.isHidden = false
+                                self.passwordLabel.isHidden = false
+                                self.loginButton.isHidden = false
+                                self.errorLabel.isHidden = false
+                            }
+                        }
+                    });
             } else if let error = error {
                 print(error.localizedDescription)
             }
